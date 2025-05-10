fdc6d55b5f0d7cb38fde3d6f0a8f30bf0f368cb0
Meghanab28 <bmeghana2801@gmail.com>
2025-05-10 10:47:33 +0530

    Initial commit

--- obj/Debug/net9.0/SignalRChat.AssemblyInfo.cs
+++ obj/Debug/net9.0/SignalRChat.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SignalRChat")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38b629477cf59ab35d0a7e0dbc00e3dab36adc7c")]
 [assembly: System.Reflection.AssemblyProductAttribute("SignalRChat")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SignalRChat")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
